3723c9c5eea0632b1aaa2e8a8ba53009ac964193
Clarenskyy <150917878+Clarenskyy@users.noreply.github.com>
2024-07-12 23:15:45 +0800

    Update Debt List System (Final).AssemblyInfo.cs

--- obj/Debug/net8.0/Debt List System (Final).AssemblyInfo.cs
+++ obj/Debug/net8.0/Debt List System (Final).AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Debt List System (Final)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+258e58bb563827b6c3db69d584659d696baa940d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Debt List System (Final)")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Debt List System (Final)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
