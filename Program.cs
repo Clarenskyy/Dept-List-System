a073299f478d8a3228c0796b0f59f62ef339c2f0
Clarenskyy <150917878+Clarenskyy@users.noreply.github.com>
2024-07-13 12:44:40 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ namespace Dept_List_System
             //done - possible limit on the number of debtors to be added in the debtor list
             //done - once a credit is reduced all the items in the list will be removed and will display the remaining balance if there is any
             //done - errors: when exiting from the credit list it double declares
-            //cant do anything about it - cant add the same price or same name
+            //fixed - cant add the same price or same name
             //done - add deptor function
             //error removed - error 3 can only do all the functions on debtor 1
             
